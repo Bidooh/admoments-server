24d797254577b917fa5d403ac52f5e1d0fa7274a
Bidooh Ltd <43378494+Bidooh@users.noreply.github.com>
2019-01-22 16:30:41 +0000

    Add files via upload

---
+++ SetBackgroundColourOperation.cs
@@ -0,0 +1,23 @@
+﻿using AdMoments.Domain.Interfaces;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace AdMoments.Business.ImageProcessing
+{
+    public class SetBackgroundColourOperation : IImageOperation
+    {
+        private readonly Rgba32 _bgColour;
+
+        public SetBackgroundColourOperation(Rgba32 bgColour)
+        {
+            _bgColour = bgColour;
+        }
+
+        public Image<Rgba32> Apply(Image<Rgba32> inImage)
+        {
+            inImage.Mutate(i => i.BackgroundColor(_bgColour));
+            return inImage;
+        }
+    }
+}
